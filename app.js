e9e15e9782028d6e5dec3d02a68257aee5318254
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-08-22 00:01:11 +0530

    cors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 
+const cors = require("cors");
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
@@ -12,13 +14,13 @@ if (process.env.NODE_ENV !== "production") {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors());
 // import routes
 
 const user = require("./routes/user");
 
-
 // using routes
-app.use("/api/v1",user)
+app.use("/api/v1", user);
 
 module.exports = app;
